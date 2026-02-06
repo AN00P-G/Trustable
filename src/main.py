45bcc9dd303380910bf36cadc030946596e62a7a
AN00P-G <guragain01@alleghney.edu>
2026-02-06 01:54:00 -0500

    added loop

--- src/main.py
+++ src/main.py
@@ -4,13 +4,17 @@ from model import pipeline
 def main():
     print("-" * 100)
     print("\nHello from trustable!")
-    
-    sample_sms = [str(input("Enter an SMS message to classify as spam or ham: "))]
-    pred = pipeline.predict(sample_sms)
-    prob = pipeline.predict_proba(sample_sms)
+    while True: 
+        sample_sms = [str(input("\nEnter an SMS message to classify as spam or ham (\"quit\" to exit): "))]
+        if sample_sms[0].lower() == "quit":
+            print("Exiting the application.")
+            break
+        else:
+            pred = pipeline.predict(sample_sms)
+            prob = pipeline.predict_proba(sample_sms)
 
-    print(f"\nSMS: {sample_sms[0]}")
-    print(f"Predicted: {pred[0]}, Trust score: {1 - prob[0][1]:.2f}")
+            print(f"\nSMS: {sample_sms[0]}")
+            print(f"Predicted: {pred[0]}, Trust score: {1 - prob[0][1]:.2f}")
 
 
 if __name__ == "__main__":
